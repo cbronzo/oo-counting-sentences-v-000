bba46ee34c34407be700c0a95714a2cab95d9c5c
Catherine Bronzo <cbronzo@mac.com>
2018-06-28 02:24:08 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,10 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self.end_with("?")
+      true
+    else
+      false
+    end
 
   end
 
